40f7542223b0b20d3b644afb1060a26ba68b70c2
Santhosh Kumar S <santhoshkumars2901@gmail.com>
2026-09-24 17:31:46 +0530

    fix(driver-auth): make handleSignupSubmit async to resolve Vite build error

--- src/pages/DriverAuthPage.jsx
+++ src/pages/DriverAuthPage.jsx
@@ -150,7 +150,7 @@ export default function DriverAuthPage({
   };
 
   // Signup submission
-  const handleSignupSubmit = (e) => {
+  const handleSignupSubmit = async (e) => {
     e.preventDefault();
     setErrorMessage('');
     setSuccessMessage('');
